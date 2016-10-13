5dbf81ddd5151539b9ab9bf9eccd13b8fdd499c9
magratheazaphod <jessed@berkeley.edu>
2016-10-12 23:58:29 -0700

    working on tilebag class some more.

--- Tilebag.py
+++ Tilebag.py
@@ -6,29 +6,32 @@
 #           -Exchange (Replace inputted tiles with new random tiles
 
 import Tile
+import numpy.random as rnd
 
 class Tilebag:
     
-    TileDistribution = {'0': 5, '1': 6,  '2': 6,  '3': 5, '4': 5,  '5': 4, \
+    BaseTileDistribution = {'0': 5, '1': 6,  '2': 6,  '3': 5, '4': 5,  '5': 4, \
                         '6': 4, '7': 4,  '8': 4,  '9': 4,'10': 2, '11': 1, \
                        '12': 2,'13': 1, '14': 1, '15': 1,'16': 1, '17': 1, \
                        '18': 1,'19': 1, '20': 1,  '+': 4, '-': 4,  '?': 4, \
                         '*': 4, '/': 4,'+|-': 5,'*|/': 4, '=': 11}
     
-    def __init__(self): #creates empty tilebag object
-        self.InBag = []
+    def __init__(self): #creates a zero dictionary showing current tiles in tilebag, and also stores all current tiles as array
         
-    def AddTileToBag(self): #checks if we're somehow trying to add a tile beyond what's supposed to be in the bag
-        self.InBag.append(Tile.Tile(k))
-
-    def FillBag(self):
+        self.TilesInBag = dict.fromkeys(self.BaseTileDistribution, 0)
         
-        for k, v in self.TileDistribution.items():
-            for i in range(v):
-                self.AddTileToBag(k)
+    def FillBag(self): #set bag to reference tile distribution
                 
-    def DrawTile(self):
+        self.TilesInBag = self.BaseTileDistribution    
         
-        return 0
+    def AddTileToBag(self, Denomination): #checks if we're somehow trying to add a tile beyond what's supposed to be in the bag
+        self.Distribution[Denomination] += 1
+                
+    def DrawTile(self, TileDesired = 'rand'):
+
+        #if thing in self: some_list.remove(thing)
+            
+        TileDrawn = rnd.choice(self.TilesInBag)
+        self.Distribution[TileDrawn.POT] -= 1
         
-    #def SwapTiles(self):
+    #def SwapTiles(self):        
